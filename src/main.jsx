78f66d6711d424834356cce7124dd750e7a1d31c
LalaIAm <lthorud12@gmail.com>
2024-04-11 20:59:47 -0500

    f

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import './assets/scss/styles.scss'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename='/bds-chat'>
       <App />
     </BrowserRouter>
   </React.StrictMode>,
--- vite.config.js
+++ vite.config.js
@@ -6,9 +6,7 @@ import fs from 'fs/promises';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  build: {
-   outdir: 'build'
- },
+  base: '/bds-chat/',
   plugins: [ react()],
   resolve: {
     alias: {
